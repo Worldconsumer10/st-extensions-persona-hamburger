83b6c2b3b258a1b98b82e9cfce22429f5d46553a
William PLT <William PLT@DESKTOP-AMV049F>
2024-01-26 02:44:36 +1000

    Update

--- index.js
+++ index.js
@@ -10,45 +10,45 @@ const defaultSettings = {
 // Keep track of where your extension is located, name should match repo name
 const extensionName = "st-extension-transformations";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
-var extensionSettings = extension_settings[extensionName];
+var extensionSettings = extension_settings[extensionName]
 
-if (typeof(extensionSettings) == "undefined"){
-  Object.assign(extensionSettings,{})
+var currentChat = getContext().getCurrentChatId();
+
+eventSource.on(event_types.CHAT_CHANGED, onChatChanged);
+
+function onChatChanged(){
+  currentChat = getContext().getCurrentChatId()
+  reset()
 }
 
-var chatExtensionSettings = extensionSettings[getContext().getCurrentChatId()] = defaultSettings
-
-console.log(chatExtensionSettings)
-
-function saveSettings(){
-  extensionSettings[getContext().getCurrentChatId()] = chatExtensionSettings
-  extension_settings = extensionSettings;
-  saveSettingsDebounced()
-  console.log(chatExtensionSettings)
-}
-
-if (typeof chatExtensionSettings == "undefined"){
-  chatExtensionSettings = defaultSettings;
-}
 
 async function loadSettings() {
-  //Load the settings
 
-  $("#character_prompt_override_setting").val(chatExtensionSettings.newDescription)
+  $("#character_prompt_override_setting").val(extensionSettings.newDescription)
 
 }
 
 function onPromptInput(){
   var promptVal = $("#character_prompt_override_setting").val();
-  chatExtensionSettings.newDescription = promptVal;
-  saveSettings();
+  extensionSettings.newDescription = promptVal;
+  saveSettingsDebounced();
 }
 
-jQuery(async () => {
-  const settingsHtml = await $.get(`${extensionFolderPath}/main.html`);
-  $("#extensions_settings").append(settingsHtml);
+reset()
 
-  $("#character_prompt_override_setting").on("input",onPromptInput)
-
-  loadSettings();
-});
+function reset(){
+  jQuery(async () => {
+    const settingsHtml = await $.get(`${extensionFolderPath}/main.html`);
+    const noChatSettingsHtml = await $.get(`${extensionFolderPath}/noChat.html`);
+  
+    if (typeof currentChat == "undefined"){
+      $("#extensions_settings").append(noChatSettingsHtml);
+    } else {
+      $("#extensions_settings").append(settingsHtml);
+    }
+  
+    $("#character_prompt_override_setting").on("input",onPromptInput)
+  
+    loadSettings();
+  });
+}
