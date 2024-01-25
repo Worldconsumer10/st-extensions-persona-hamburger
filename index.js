45892ee8c9b7efcdff90fbf0b28d3fae7e59af0f
SUCC-u-lent <william.is.ney@gmail.com>
2024-01-25 19:28:45 +1000

    Init

---
+++ index.js
@@ -0,0 +1,37 @@
+import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
+
+import { saveSettingsDebounced } from "../../../../script.js";
+
+const extensionName = "st-extension-transformation";
+const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
+const extensionSettings = extension_settings[extensionName];
+const defaultSettings = {};
+
+async function loadSettings() {
+  extension_settings[extensionName] = extension_settings[extensionName] || {};
+  if (Object.keys(extension_settings[extensionName]).length === 0) {
+    Object.assign(extension_settings[extensionName], defaultSettings);
+  }
+  $("#example_setting").prop("checked", extension_settings[extensionName].example_setting).trigger("input");
+}
+
+function onExampleInput(event) {
+  const value = Boolean($(event.target).prop("checked"));
+  extension_settings[extensionName].example_setting = value;
+  saveSettingsDebounced();
+}
+
+function onButtonClick() {
+  toastr.info(
+    `The checkbox is ${extension_settings[extensionName].example_setting ? "checked" : "not checked"}`,
+    "A popup appeared because you clicked the button!"
+  );
+}
+
+jQuery(async () => {
+  const settingsHtml = await $.get(`${extensionFolderPath}/example.html`);
+  $("#extensions_settings").append(settingsHtml);
+  $("#my_button").on("click", onButtonClick);
+  $("#example_setting").on("input", onExampleInput);
+  loadSettings();
+});
