f41ce09a8a41cde9a068acd552a05e483e705296
SUCC-u-lent <william.is.ney@gmail.com>
2024-01-25 20:32:20 +1000

    Update

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ function onButtonClick() {
 jQuery(async () => {
   const settingsHtml = await $.get(`${extensionFolderPath}/menuentry.html`);
   $("#extensions_settings").append(settingsHtml);
+  $("#my_button").on("click", onButtonClick);
   $("#advanced_character").on("input", onExampleInput);
   loadSettings();
 });
