4f01ab7f78128b00c09162760d3d2f08f055a2ef
William PLT <William PLT@DESKTOP-AMV049F>
2024-01-26 01:19:01 +1000

    Update

--- index.js
+++ index.js
@@ -66,25 +66,6 @@ var character_trans = false
 
 eventSource.on(event_types.MESSAGE_RECEIVED,handleIncomingMessage)
 
-function getSaveLocation(){
-  var context = getContext()
-  if (context == null || context == undefined){
-    return extensionName;
-  } else if (context.chatId == null || context.chatId == undefined){
-    return extensionName;
-  }
-  return extensionName + context.chatId;
-}
-function getValue(value, defaultValue) {
-  var saveLocation = getSaveLocation()
-  if (saveFile[saveLocation] == null || saveFile[saveLocation] == undefined) {
-      return defaultValue;
-  } else if (saveFile[saveLocation][value] == null || saveFile[saveLocation][value] == undefined) {
-      return defaultValue;
-  }
-  return saveFile[saveLocation][value];
-}
-
 //Self Note: extension_settings[getSaveLocation()].adv_inputs is erroring somewhere
 
 function handleIncomingMessage(){
@@ -120,7 +101,7 @@ function handleIncomingMessage(){
 }
 
 function getStartTerms(){
-  if (saveFile[getSaveLocation()].adv_inputs){
+  if (saveFile.adv_inputs){
     var result = $("#start_trigger_settings").val()
     return result.split(",");
   } else {
@@ -130,7 +111,7 @@ function getStartTerms(){
 }
 async function getEndTerms(){
   
-  if (saveFile[getSaveLocation()].adv_inputs){
+  if (saveFile.adv_inputs){
     var result = $("#end_trigger_settings").val()
     return result.split(",");
   } else {
@@ -148,7 +129,7 @@ function getBasicTransforms(){
       str = str + "|" + element.trim()
     }
   });
-  if (saveFile[getSaveLocation()].adv_inputs){
+  if (saveFile.adv_inputs){
     $("#end_trigger_settings").val().split(",").forEach(element => {
       if (str == ""){
         str = element.trim()
@@ -191,8 +172,7 @@ function getLastElement(t){
 }
 
 function validateSettings(){
-  var saveLocation = getSaveLocation();
-  var saveData = saveFile[saveLocation];
+  var saveData = saveFile;
   if (typeof saveData == "undefined")
   { saveData = defaultSettings }
   else{
@@ -206,13 +186,12 @@ function validateSettings(){
       }
     }
   }
-  saveFile[saveLocation]=saveData
+  saveFile=saveData
 }
 
 async function loadSettings() {
-  var saveLocation = getSaveLocation();
-  if (Object.keys(saveFile[saveLocation]).length === 0) {
-    Object.assign(saveFile[saveLocation], defaultSettings);
+  if (Object.keys(saveFile).length === 0) {
+    Object.assign(saveFile, defaultSettings);
     updateSettingsSave();
     validateSettings();
   } else {
@@ -220,12 +199,12 @@ async function loadSettings() {
     validateSettings();
   }
 
-  advanced_character = saveFile[saveLocation].adv_character
-  advanced_inputs = saveFile[saveLocation].adv_inputs
-  list_basic_keys = saveFile[saveLocation].basic_keys
-  list_start_keys = saveFile[saveLocation].start_keys
-  list_end_keys = saveFile[saveLocation].adv_character
-  character_trans = saveFile[saveLocation].char_trans
+  advanced_character = saveFile.adv_character
+  advanced_inputs = saveFile.adv_inputs
+  list_basic_keys = saveFile.basic_keys
+  list_start_keys = saveFile.start_keys
+  list_end_keys = saveFile.adv_character
+  character_trans = saveFile.char_trans
 
   console.log(
     `Loaded Settings:\nAdvanced Character Enabled: ${advanced_character}\n`+
@@ -246,7 +225,7 @@ async function loadSettings() {
 function onAdvPlayerInput(event) {
   var saveLocation = getSaveLocation()
   const value = Boolean($(event.target).prop("checked"));
-  saveFile[saveLocation].adv_character = value;
+  saveFile.adv_character = value;
   advanced_character = value
   saveSettingsDebounced();
   if (value){
@@ -262,7 +241,7 @@ function onAdvPlayerInput(event) {
 function onAdvInputsInput(event) {
   var saveLocation = getSaveLocation()
   const value = Boolean($(event.target).prop("checked"));
-  saveFile[saveLocation].adv_inputs = value;
+  saveFile.adv_inputs = value;
   advanced_inputs = value;
   saveSettingsDebounced();
   if (value){
@@ -273,22 +252,21 @@ function onAdvInputsInput(event) {
 }
 
 function setCharTransformed(state){
-  saveFile[getSaveLocation()].char_trans = state;
+  saveFile.char_trans = state;
 }
 function onTextChanged(){
-  var saveLocation = getSaveLocation();
   if (advanced_inputs){
     list_start_keys = $("start_trigger_settings").val()
     list_end_keys = $("end_trigger_settings").val()
-    saveFile[saveLocation].start_keys = list_start_keys
-    saveFile[saveLocation].end_keys = list_end_keys
+    saveFile.start_keys = list_start_keys
+    saveFile.end_keys = list_end_keys
   } else {
     list_basic_keys = $("basic_trigger_settings").val()
-    saveFile[saveLocation].basic_keys = list_basic_keys
+    saveFile.basic_keys = list_basic_keys
   }
 }
 function toggleTransformed(){
-  setCharTransformed(!saveFile[getSaveLocation()].char_trans)
+  setCharTransformed(!saveFile.char_trans)
 }
 var lastCheck = {}
 function runUpdateCheck(){
@@ -299,7 +277,7 @@ function runUpdateCheck(){
 }
 
 function onForceRevert(){
-  saveFile[getSaveLocation()].char_trans=false
+  saveFile.char_trans=false
   reset()
   toastr.warning(
     `This did not force revert your character in context, this only made the script believe the character is not transformed`,
@@ -343,19 +321,19 @@ function reset(wasInit){
     const charTransformed = await $.get(`${extensionFolderPath}/htmlelements/add/chartrans.html`);
     const charNotTransformed = await $.get(`${extensionFolderPath}/htmlelements/add/charnorm.html`);
 
-    if (saveFile[saveLocation].char_trans){
+    if (saveFile.char_trans){
       $("#table_container").append(charTransformed);
     } else {
       $("#table_container").append(charNotTransformed);
     }
 
-    if (saveFile[saveLocation].char_trans){
+    if (saveFile.char_trans){
       $("#force_revert_setting").removeAttr("disabled");
     } else {
       $("#force_revert_setting").attr("disabled", "");
     }
 
-    if (saveFile[saveLocation].adv_inputs){
+    if (saveFile.adv_inputs){
       $("#table_container").append(tranTrigAdvancedStart);
       $("#table_container").append(tranTrigAdvancedEnd);
       $("#start_trigger_settings").on("input",onTextChanged)
@@ -368,7 +346,7 @@ function reset(wasInit){
       $("#basic_trigger_settings").prop("value",extension_settings[saveLocation].basic_keys)
     }
 
-    if (saveFile[saveLocation].adv_character)
+    if (saveFile.adv_character)
     {
       $("#table_container").append(tranAdv);
     } else {
