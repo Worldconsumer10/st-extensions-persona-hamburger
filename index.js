e50af0fd7d460f85f7d7be1b95d26a412bb5e4a8
William PLT <William PLT@DESKTOP-AMV049F>
2024-01-27 00:24:44 +1000

    Update

--- index.js
+++ index.js
@@ -229,8 +229,8 @@ function reset(){
       var selectedValue = $("#character_transform_dropdown").val();
       if(selectedValue && selectedValue != "none") {
         $('#character_transform_dropdown option[value="' + selectedValue + '"]').remove();
-        console.log(extensionSettings[currentChat].transformedKeywords)
         extensionSettings[currentChat].transformedKeywords = extensionSettings[currentChat].transformedKeywords.replace(new RegExp(selectedValue+";;","g"),"")
+        saveSettingsDebounced();
       }
     })
     $("#character_untransform_dropdown").change(()=>{
@@ -238,6 +238,7 @@ function reset(){
       if(selectedValue && selectedValue != "none") {
         $('#character_untransform_dropdown option[value="' + selectedValue + '"]').remove();
         extensionSettings[currentChat].untransformedKeywords = extensionSettings[currentChat].untransformedKeywords.replace(new RegExp(selectedValue+";;","g"),"")
+        saveSettingsDebounced();
       }
     })
 
