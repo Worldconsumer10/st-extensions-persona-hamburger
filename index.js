ccaa60c2f7a16f707f1cc5af338c53ecd9989f04
William PLT <William PLT@DESKTOP-AMV049F>
2024-01-26 22:05:54 +1000

    Update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function onChatChanged(){
 
 async function generateInterceptor(chat){
   //Called when a generation is being proccessed
-  setExtensionPrompt(extension_prompt_tag,"[ This is an example prompt ]",0,5,false)
+  setExtensionPrompt(extension_prompt_tag,"[ This is an example prompt ]",-1,20,false)
   console.log("Added Extension Prompt")
 }
 
