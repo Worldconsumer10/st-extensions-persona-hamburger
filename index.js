c2fc837cee383f47d699f5f1a4da6fe55e858d8a
SUCC-u-lent <william.is.ney@gmail.com>
2024-01-26 19:41:12 +1000

    Update

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function onMessageSent(msgID){
 
           var history = splitter.reverse()[0]
 
-          console.log("Context: ",chatContext,"\nDescription: ",description)
+          console.log("Context:\n",chatContext,"\nDescription:\n",description)
 
         }catch(ex){
           console.error(ex)
