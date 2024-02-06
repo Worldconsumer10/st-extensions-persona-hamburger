ef8b70b2e28c0e445a1a84f76f6a14ec44771a25
William PLT <William PLT@DESKTOP-AMV049F>
2024-02-06 21:37:54 +1000

    Update

--- hamburger-framework.js
+++ hamburger-framework.js
@@ -2,6 +2,11 @@ function loadFramework() {
     $(async()=>{
         var hambDiv = document.createElement("div")
         var personaElement = $("#persona-management-block")
+        if (personaElement == undefined){
+            toastr.warn("FAILED","Hamburger could not be added as framework was loaded too early")
+            console.warn("[Hamburger Framework] Loaded too early! Failed to add!")
+            return
+        }
         personaElement.firstChild.firstChild.appendChild(hambDiv)
     });
 }
