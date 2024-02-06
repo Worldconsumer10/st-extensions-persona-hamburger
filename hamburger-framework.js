0857184d9691a3c361b6d51e5e64c384c09acc7d
William PLT <William PLT@DESKTOP-AMV049F>
2024-02-06 21:40:04 +1000

    Update

--- hamburger-framework.js
+++ hamburger-framework.js
@@ -1,7 +1,7 @@
 function loadFramework() {
     $(async()=>{
         var hambDiv = document.createElement("div")
-        var personaElement = $("#persona-management-block")
+        var personaElement = $("#persona-management-block")[0]
         if (typeof personaElement == "undefined"){
             toastr.warn("FAILED","Hamburger could not be added as framework was loaded too early")
             console.warn("[Hamburger Framework] Loaded too early! Failed to add!")
