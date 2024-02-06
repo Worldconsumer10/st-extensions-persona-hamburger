6e38fc54697685a52667c6f9948c903d27fec9ea
William PLT <William PLT@DESKTOP-AMV049F>
2024-02-06 22:09:55 +1000

    Update

--- hamburger-framework.js
+++ hamburger-framework.js
@@ -2,8 +2,15 @@ function loadFramework() {
     $(async()=>{
         var hambDiv = document.createElement("div")
 
-        hambDiv.className = "tag showTagList actionable margin-right-10px"
-        hambDiv.style = "background-color: rgba(150, 100, 100, 0.5);"
+        hambDiv.style = "width:100%;height:auto;max-width:20px"
+
+        var buns = document.createElement("div")
+
+        buns.style = "width:100%; height:2px; background-color:white;border-radius:3px;margin-top:3px"
+
+        var meat = document.createElement("div")
+
+        meat.style = "width:100%; height:2px; background-color:white;margin-top:3px;margin-bottom:3px; border-radius:3px"
 
         var personaElement = $("#persona-management-block")[0]
         if (typeof personaElement == "undefined"){
@@ -14,6 +21,11 @@ function loadFramework() {
         var personaTab = personaElement.children[0].children[0]
         var lastNode = personaTab.children[personaTab.children.length-1].cloneNode(true)
         personaTab.children[personaTab.children.length-1].remove()
+
+        hambDiv.appendChild(buns)
+        hambDiv.appendChild(meat)
+        hambDiv.appendChild(buns)
+
         personaTab.appendChild(hambDiv)
         personaTab.appendChild(lastNode)
     });
