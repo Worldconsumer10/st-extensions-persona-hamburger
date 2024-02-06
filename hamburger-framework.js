a51400c52a40fad02cc011305225c68752c3a28a
William PLT <William PLT@DESKTOP-AMV049F>
2024-02-06 21:33:30 +1000

    Update

--- hamburger-framework.js
+++ hamburger-framework.js
@@ -1,5 +1,5 @@
 function loadFramework() {
-    jQuery(async()=>{
+    $(async()=>{
         var hambDiv = document.createElement("div")
         var personaElement = $("#persona-management-block") = new HTMLDivElement()
         personaElement.firstChild.firstChild.appendChild(hambDiv)
